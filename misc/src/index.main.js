5768df29d233d69c2600a1b4e6a6b1fe874e4936
aq <aqgtssize@gmail.com>
2016-11-25 14:26:00 +0900

    自分用の雑多なスクリプト置き場を作った

---
+++ misc/src/index.main.js
@@ -0,0 +1,86 @@
+import "babel-polyfill";
+import PMX from "./pmx";
+import VMD from "./vmd";
+import PMXUtils from "./pmx-utils";
+import BinaryUtils from "./binary-utils";
+import Vector2 from "./vector2";
+import Vector3 from "./vector3";
+import Plane from "./plane";
+import BezierCurve from "./bezier-curve";
+import Quaternion from "./quaternion";
+import MyMath from "./my-math";
+import TextAreaWrapper from "./text-area-wrapper";
+
+{
+  let log = null;
+  new Vue({
+    el: "#panorama360_motion",
+    data: {
+      x: "0",
+      y: "0",
+      z: "0",
+      isLoading: true,
+      isProcessing: false
+    },
+    computed: {
+    },
+    methods: {
+      createMotion: async function () {
+        this.isProcessing = true;
+        try {
+          log.clear();
+  
+          const linearBezier = new BezierCurve(
+            new Vector2(0, 0),
+            new Vector2(20 / 127, 20 / 127),
+            new Vector2(107 / 127, 107 / 127),
+            new Vector2(1, 1)
+          );
+
+          const motion = new VMD(VMD.CAMERA_MODEL_NAME, {
+            bone: [],
+            morph: [],
+            camera: new Array(9 * 18).fill().map((_, frameNumber) => {
+              const yaw = frameNumber % 18 * 20 * Math.PI / 180;
+              const pitch = (Math.floor(frameNumber / 18) * 20 - 80) * Math.PI / 180;
+              return new VMD.CameraKeyFrame(
+                frameNumber,
+                0,
+                new Vector3(Number(this.x), Number(this.y), Number(this.z)),
+                Quaternion.yxzEuler(yaw, pitch, 0),
+                {
+                  x: linearBezier,
+                  y: linearBezier,
+                  z: linearBezier,
+                  rotation: linearBezier,
+                  distance: linearBezier,
+                  angleOfView: linearBezier
+                },
+                60 * Math.PI / 180,
+                true
+              );
+            }),
+            light: [],
+            selfShadow: [],
+            showIK: []
+          });
+  
+          BinaryUtils.saveBinaryAsFile(motion.write(), "camera.vmd");
+
+          log.appendAsync("モーションの作成に成功しました");
+        } catch (error) {
+          await log.appendAsync(`[Error]${error}`);
+          throw error;
+        } finally {
+          this.isProcessing = false;
+        }
+      }
+    },
+    watch: {
+    },
+    mounted() {
+      log = new TextAreaWrapper(document.getElementById("panorama360_motion_log"));
+      this.isLoading = false;
+    }
+  });
+}
--- shared/src/vmd.js
+++ shared/src/vmd.js
@@ -1,6 +1,5 @@
 import Packer from "./packer";
 
-const CAMERA_MODEL_NAME = "カメラ・照明\0on Data";
 export default class VMD {
   constructor(modelName, keyFrames) {
     this.modelName = modelName;
@@ -12,8 +11,8 @@ export default class VMD {
       offset: 0
     };
     Packer.writeNullTerminatedString(io, "Vocaloid Motion Data 0002", 30, "shift_jis");
-    if (this.modelName == CAMERA_MODEL_NAME) {
-      Packer.writeString(io, CAMERA_MODEL_NAME, "shift_jis");
+    if (this.modelName == this.constructor.CAMERA_MODEL_NAME) {
+      Packer.writeString(io, this.constructor.CAMERA_MODEL_NAME, "shift_jis");
     } else {
       Packer.writeNullTerminatedString(io, this.modelName, 20, "shift_jis");
     }
@@ -46,6 +45,7 @@ export default class VMD {
     return new Uint8Array(io.view.buffer, 0, io.offset);
   }
 }
+VMD.CAMERA_MODEL_NAME = "カメラ・照明\0on Data";
 VMD.BoneKeyFrame = class BoneKeyFrame {
   constructor(boneName, frameNumber, position, quaternion, bezierCurves) {
     this.boneName = boneName;
@@ -94,5 +94,39 @@ VMD.BoneKeyFrame = class BoneKeyFrame {
       this.bezierCurves.z.controlPoints[2].y, this.bezierCurves.rotation.controlPoints[2].y,
       0, 0, 0
     ].map(v => v * 127));
+  }
+};
+VMD.CameraKeyFrame = class CameraKeyFrame {
+  constructor(frameNumber, distance, position, quaternion, bezierCurves, angleOfView, isPerspectiveMode) {
+    this.frameNumber = frameNumber;
+    this.distance = distance;
+    this.position = position;
+    this.quaternion = quaternion;
+    this.bezierCurves = bezierCurves;
+    this.angleOfView = angleOfView;
+    this.isPerspectiveMode = isPerspectiveMode;
+  }
+  write(io) {
+    const [yaw, pitch, roll] = this.quaternion.yxzEulerAngles();
+    Packer.writeUint32(io, this.frameNumber);
+    Packer.writeFloat32(io, -this.distance);
+    Packer.writeFloat32Array(io, Array.from(this.position));
+    Packer.writeFloat32Array(io, [-pitch, yaw, roll]);
+    Packer.writeInt8Array(io, [
+      this.bezierCurves.x.controlPoints[1].x, this.bezierCurves.x.controlPoints[2].x,
+      this.bezierCurves.x.controlPoints[1].y, this.bezierCurves.x.controlPoints[2].y,
+      this.bezierCurves.y.controlPoints[1].x, this.bezierCurves.y.controlPoints[2].x,
+      this.bezierCurves.y.controlPoints[1].y, this.bezierCurves.y.controlPoints[2].y,
+      this.bezierCurves.z.controlPoints[1].x, this.bezierCurves.z.controlPoints[2].x,
+      this.bezierCurves.z.controlPoints[1].y, this.bezierCurves.z.controlPoints[2].y,
+      this.bezierCurves.rotation.controlPoints[1].x, this.bezierCurves.rotation.controlPoints[2].x,
+      this.bezierCurves.rotation.controlPoints[1].y, this.bezierCurves.rotation.controlPoints[2].y,
+      this.bezierCurves.distance.controlPoints[1].x, this.bezierCurves.distance.controlPoints[2].x,
+      this.bezierCurves.distance.controlPoints[1].y, this.bezierCurves.distance.controlPoints[2].y,
+      this.bezierCurves.angleOfView.controlPoints[1].x, this.bezierCurves.angleOfView.controlPoints[2].x,
+      this.bezierCurves.angleOfView.controlPoints[1].y, this.bezierCurves.angleOfView.controlPoints[2].y
+    ].map(v => v * 127));
+    Packer.writeUint32(io, Math.round(this.angleOfView * 180 / Math.PI));
+    Packer.writeInt8(io, this.isPerspectiveMode ? 0 : 1);
   }
 };
